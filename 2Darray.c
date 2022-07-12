b328c45d7a13e8696f2986450d615479bddd03c7
pee47kay <papakowdadson@gmail.com>
2022-07-12 17:53:45 +0000

    2D arrays

---
+++ 2Darray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// 2D array
+int main()
+{
+    int A[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 8, 7, 6}};
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d", A[i][j]);
+        }
+        printf("\n");
+    }
+}
